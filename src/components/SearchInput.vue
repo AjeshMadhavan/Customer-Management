b13b015427164f06839966d78fb51c318c030289
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-07-19 11:36:08 +0530

    feat: added search input field

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-text-field
+        filled
+        :prepend-inner-icon="props.prependIcon"
+        :placeholder="props.placeholderText"
+        clearable
+        hide-details
+    />
+</template>
+
+<script lang="ts" setup>
+import {defineProps} from 'vue'
+
+const props = defineProps({
+    placeholderText: {
+        type: String,
+        default: "Search"
+    },
+    prependIcon: {
+        type: String,
+        default: 'mdi-magnify' 
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.v-input__icon {
+    margin-right: 20px !important;
+}
+</style>
