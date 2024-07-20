9ce8accb5c90760ade944fce69f445e313c16a69
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-07-20 10:47:23 +0530

    refactor: added clear icon

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,15 +1,25 @@
 <template>
+<div class="search-field">
     <v-text-field
         filled
-        :prepend-inner-icon="props.prependIcon"
         :placeholder="props.placeholderText"
         clearable
+        clear-icon="mdi-close-circle"
         hide-details
-    />
+        loader-height="3"
+    >
+        
+        <template v-slot:prepend-inner>
+            <v-icon class="prepend-icon">mdi-magnify</v-icon>
+        </template>
+    </v-text-field>
+</div>
 </template>
 
 <script lang="ts" setup>
+import colors from "vuetify/lib/util/colors"
 import {defineProps} from 'vue'
+
 
 const props = defineProps({
     placeholderText: {
@@ -24,7 +34,14 @@ const props = defineProps({
 </script>
 
 <style lang="scss" scoped>
-.v-input__icon {
-    margin-right: 20px !important;
+.search-field {
+    width: 400px;
+}
+.prepend-icon {
+    margin-right: 10px;
+}
+
+.v-text-field__slot {
+    margin-left: 20px !important;
 }
 </style>
