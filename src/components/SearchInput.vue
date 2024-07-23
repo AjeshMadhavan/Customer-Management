19e5f65be31994ef904a3548adc87aa365487beb
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-07-23 10:10:32 +0530

    refactor: updated clear icon

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,47 +1,59 @@
 <template>
-<div class="search-field">
+  <div class="search-field">
     <v-text-field
-        filled
-        :placeholder="props.placeholderText"
-        clearable
-        clear-icon="mdi-close-circle"
-        hide-details
-        loader-height="3"
+      v-model="inputValue"
+      filled
+      :placeholder="props.placeholderText"
+      hide-details
+      loader-height="3"
+      class="search-bar"
+      :height="38"
     >
-        
-        <template v-slot:prepend-inner>
-            <v-icon class="prepend-icon">mdi-magnify</v-icon>
-        </template>
+      <template v-slot:prepend-inner>
+        <v-icon class="prepend-icon">mdi-magnify</v-icon>
+      </template>
+
+      <template v-if="inputValue" v-slot:append>
+        <v-icon @click="clearText" class="clear-icon">mdi-close </v-icon>
+      </template>
     </v-text-field>
-</div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import colors from "vuetify/lib/util/colors"
-import {defineProps} from 'vue'
-
+import { defineProps, ref } from "vue";
 
 const props = defineProps({
-    placeholderText: {
-        type: String,
-        default: "Search"
-    },
-    prependIcon: {
-        type: String,
-        default: 'mdi-magnify' 
-    }
-})
+  placeholderText: {
+    type: String,
+    default: "Search",
+  },
+  prependIcon: {
+    type: String,
+    default: "mdi-magnify",
+  },
+});
+
+const inputValue = ref<string>("");
+
+const clearText = () => {
+  inputValue.value = "";
+};
 </script>
 
 <style lang="scss" scoped>
 .search-field {
-    width: 400px;
+  width: 400px;
+  height: 38px;
 }
 .prepend-icon {
-    margin-right: 10px;
+  margin-right: 10px;
 }
 
-.v-text-field__slot {
-    margin-left: 20px !important;
+.clear-icon {
+  background-color: rgba(194, 194, 194, 0.87);
+  color: rgba(117, 117, 117, 0.87);
+  border-radius: 50%;
+  font-size: 16px;
 }
 </style>
