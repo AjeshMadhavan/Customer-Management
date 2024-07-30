36f3c301f1a9ae8380ff87bb2ce5321d60c95073
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-07-30 15:09:58 +0530

    refactor: updated progress bar color and clear icon

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="search-field">
+  <div class="search-field h-8 flex">
     <v-text-field
       v-model="inputValue"
       filled
       :placeholder="props.placeholderText"
       hide-details
+      dense
+      min-height="38"
       loader-height="3"
-      class="search-bar"
-      :height="38"
+      class="!text-red-400 min-h-8"
     >
       <template v-slot:prepend-inner>
-        <v-icon class="prepend-icon">mdi-magnify</v-icon>
+        <v-icon class="mr-2.5">mdi-magnify</v-icon>
       </template>
 
       <template v-if="inputValue" v-slot:append>
-        <v-icon @click="clearText" class="clear-icon">mdi-close </v-icon>
+        <v-icon
+          @click="clearText"
+          class="clear-icon w-4 h-4 !text-base rounded-full bg-[#c2c2c2de] text-[#757575de] translate-y-[20%]"
+        >
+          mdi-close
+        </v-icon>
       </template>
     </v-text-field>
   </div>
@@ -40,20 +46,12 @@ const clearText = () => {
   inputValue.value = "";
 };
 </script>
-
 <style lang="scss" scoped>
-.search-field {
-  width: 400px;
-  height: 38px;
-}
-.prepend-icon {
-  margin-right: 10px;
+.v-text-field .v-input__control .v-input__slot {
+  min-height: 30px !important;
 }
 
-.clear-icon {
-  background-color: rgba(194, 194, 194, 0.87);
-  color: rgba(117, 117, 117, 0.87);
-  border-radius: 50%;
-  font-size: 16px;
+.v-text-field .v-input__slot {
+  min-height: 38px !important;
 }
 </style>
