30910b5f5a228a7b987bb3ecb459d76060d10d8a
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-08-05 14:51:37 +0530

    refactor: updated eslintrs.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
 module.exports = {
-  'extends': [
-    '@vue/typescript'
-  ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
