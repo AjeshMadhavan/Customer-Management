13a6e86da4743dc71b3e909aed952106a76923cc
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-08-06 15:48:16 +0530

    test: created stories for text component

---
+++ src/components/TextComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex items-center">
+    <v-icon v-if="props.prependIcon" class="!text-base/4 !text-slate-400 p-1.5">
+      {{ props.prependIcon }}
+    </v-icon>
+    <div :class="['font-normal', 'list-text', props.textContainerStyle]">
+      <span v-if="props.text" class="text-2xs">
+        {{ props.text }}
+      </span>
+      <span v-if="props.linkText" :class="['text-xs', props.linkTextStyle]">
+        {{ props.linkText }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from "vue";
+
+interface Props {
+  prependIcon?: string;
+  text?: string;
+  linkText?: string;
+  linkTextStyle?: string;
+  textContainerStyle?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  prependIcon: '',
+  text: '',
+  linkText: '',
+  linkTextStyle: '',
+  textContainerStyle: ''
+})
+
+//<text-component prependIcon="mdi-phone" textContainerStyle="px-3 py-2.5"  linkText="CTO" />
+//<text-component textContainerStyle="px-3 pb-1" text="Position" linkText="CTO" linkTextStyle="text-blue-600"/>
+</script>
+
+<style scoped lang="scss">
+.list-text {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  row-gap: 5px;
+  font-weight: 400;
+}
+</style>
