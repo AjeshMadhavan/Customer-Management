857f6be48ebf0080f7bc37aaac3a81669aa9f5b7
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-08-09 09:21:42 +0530

    refactor: formated storybook main file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -18,7 +18,7 @@ const config: StorybookConfig = {
   webpackFinal: async (config) => {
     config?.module?.rules?.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],      
+      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
     });
 
     return config;
