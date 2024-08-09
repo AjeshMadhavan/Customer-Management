3219e5e5722328ce504eff947a7144e888bb7575
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-08-09 11:00:19 +0530

    refactor: updated button unit test

--- src/test/ButtonComponent.spec.js
+++ src/test/ButtonComponent.spec.js
@@ -3,88 +3,44 @@ import { shallowMount } from "@vue/test-utils";
 
 import ButtonComponent from "../components/ButtonComponent.vue";
 
-describe("button with primary variant", () => {
-  it("renders with primary variant", () => {
+describe("test with button component", () => {
+  it("renders button component with primary variant", () => {
     const wrapper = shallowMount(ButtonComponent, {
       propsData: {
         variant: "primary",
-      },
-    });
-
-    expect(wrapper.props("variant")).toMatch("primary");
-  });
-
-  it("button contains the right class", () => {
-    const wrapper = shallowMount(ButtonComponent, {
-      propsData: {
-        variant: "primary",
-      },
-    });
-
-    expect(wrapper.classes()).toContain("primary");
-  });
-
-  it("renders text", () => {
-    const wrapper = shallowMount(ButtonComponent, {
-      propsData: {
         text: "Add Contact",
-      },
-    });
-
-    expect(wrapper.text()).toMatch("Add Contact");
-  });
-
-  it("renders icon", () => {
-    const wrapper = shallowMount(ButtonComponent, {
-      propsData: {
         icon: "mdi-plus",
       },
     });
 
+    expect(wrapper.props("variant")).toMatch("primary");
+    expect(wrapper.classes()).toContain("primary");
+    expect(wrapper.text()).toMatch("Add Contact");
     expect(wrapper.props("icon")).toMatch("mdi-plus");
   });
-});
 
-describe("button with outline variant", () => {
   it("renders button with outline variant", () => {
     const wrapper = shallowMount(ButtonComponent, {
       propsData: {
         variant: "outlined",
-      },
-    });
-
-    expect(wrapper.find(".outlined").exists()).toBeTruthy();
-  });
-
-  it("renders text", () => {
-    const wrapper = shallowMount(ButtonComponent, {
-      propsData: {
         text: "Details",
       },
     });
 
-    expect(wrapper.props("text")).toMatch("Details");
+    expect(wrapper.props("variant")).toMatch("outlined");
+    expect(wrapper.find(".outlined").exists()).toBeTruthy();
+    expect(wrapper.text()).toMatch("Details");
   });
-});
 
-describe("button with icon variant", () => {
   it("renders button with icon variant", () => {
     const wrapper = shallowMount(ButtonComponent, {
       propsData: {
         variant: "icon",
-      },
-    });
-
-    expect(wrapper.find(".icon").exists()).toBeTruthy();
-  });
-
-  it("renders icon", () => {
-    const wrapper = shallowMount(ButtonComponent, {
-      propsData: {
         icon: "mdi-magnify",
       },
     });
 
+    expect(wrapper.find(".icon").exists()).toBeTruthy();
     expect(wrapper.props("icon")).toMatch("mdi-magnify");
   });
 });
