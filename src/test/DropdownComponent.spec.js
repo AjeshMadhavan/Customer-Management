24f619f1a4a5c78b410a0b2434f8221fa1a97029
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-08-14 18:03:33 +0530

    refactor: updated component wrapper name in test file

--- src/test/DropdownComponent.spec.js
+++ src/test/DropdownComponent.spec.js
@@ -5,45 +5,45 @@ import DropdownComponent from "../components/DropdownComponent.vue";
 
 describe("test with dropdown component", () => {
   it("renders title", () => {
-    const wrapper = shallowMount(DropdownComponent, {
+    const dropdown = shallowMount(DropdownComponent, {
       propsData: {
         text: "CRM",
       },
     });
 
-    expect(wrapper.props("text")).toMatch("CRM");
+    expect(dropdown.props("text")).toMatch("CRM");
   });
 
   it("renders icon", () => {
-    const wrapper = shallowMount(DropdownComponent, {
+    const dropdown = shallowMount(DropdownComponent, {
       propsData: {
         prependIcon: "mdi-account",
       },
     });
 
-    expect(wrapper.props("prependIcon")).toMatch("mdi-account");
+    expect(dropdown.props("prependIcon")).toMatch("mdi-account");
   });
 
   it("render dropdown style", () => {
     const dropdownStyle = "bg-white shadow-[0_2px_4px_0_rgba(0,0,0,0.2)]";
-    const wrapper = shallowMount(DropdownComponent, {
+    const dropdown = shallowMount(DropdownComponent, {
       propsData: {
         dropdownContainerStyle: dropdownStyle,
       },
     });
 
-    expect(wrapper.classes()).toContain(...dropdownStyle.split(" "));
+    expect(dropdown.classes()).toContain(...dropdownStyle.split(" "));
   });
 
   it("renders button style", () => {
     const dropdownButtonStyles = "!font-bold pt-[9px] pr-[16px] pb-[9px]";
-    const wrapper = shallowMount(DropdownComponent, {
+    const dropdown = shallowMount(DropdownComponent, {
       propsData: {
         dropdownButtonStyle: dropdownButtonStyles,
       },
     });
 
-    expect(wrapper.find(".toggle-button").classes()).toContain(
+    expect(dropdown.find(".toggle-button").classes()).toContain(
       ...dropdownButtonStyles.split(" ")
     );
   });
