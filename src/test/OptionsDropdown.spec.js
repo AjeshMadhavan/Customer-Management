4d923397c768ddcb2b83031de459a4d61e338da2
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-08-14 18:11:29 +0530

    refactor: updated wrapper varible name in test file

--- src/test/OptionsDropdown.spec.js
+++ src/test/OptionsDropdown.spec.js
@@ -5,55 +5,55 @@ import OptionsDropdown from "../components/OptionsDropdown.vue";
 
 describe("test with options dropdown", () => {
   it("renders text", () => {
-    const wrapper = shallowMount(OptionsDropdown, {
+    const optionsDropdown = shallowMount(OptionsDropdown, {
       propsData: {
         text: "Export Data",
       },
     });
 
-    expect(wrapper.find(".uppercase").text()).toMatch("Export Data");
+    expect(optionsDropdown.find(".uppercase").text()).toMatch("Export Data");
   });
 
   it("renders icon", () => {
-    const wrapper = shallowMount(OptionsDropdown, {
+    const optionsDropdown = shallowMount(OptionsDropdown, {
       propsData: {
         appendIcon: "mdi-menu-up",
       },
     });
 
-    expect(wrapper.find("v-icon").text()).toMatch("mdi-menu-up");
+    expect(optionsDropdown.find("v-icon").text()).toMatch("mdi-menu-up");
   });
 
   it("renders button style", () => {
-    const wrapper = shallowMount(OptionsDropdown, {
+    const optionsDropdown = shallowMount(OptionsDropdown, {
       propsData: {
         optionButtonStyle: "bg-blue",
       },
     });
 
-    expect(wrapper.find("button").classes("bg-blue")).toBeTruthy();
+    expect(optionsDropdown.find("button").classes("bg-blue")).toBeTruthy();
   });
 
   it("renders image src", () => {
     const imageSrc = "https://images.unsplash.com/photo-1714891203404";
 
-    const wrapper = shallowMount(OptionsDropdown, {
+    const optionsDropdown = shallowMount(OptionsDropdown, {
       propsData: {
         imageUrl: imageSrc,
       },
     });
 
-    expect(wrapper.find(".profile-image").attributes("src")).toMatch(imageSrc);
+    expect(optionsDropdown.find(".profile-image").attributes("src")).toMatch(imageSrc);
   });
 
   it("renders slot", () => {
-    const wrapper = shallowMount(OptionsDropdown, {
+    const optionsDropdown = shallowMount(OptionsDropdown, {
       slots: {
         optionsSlot: "<div>Hello World</div>",
       },
     });
 
-    expect(wrapper.vm.$slots.optionsSlot[0].children[0].text).toMatch(
+    expect(optionsDropdown.vm.$slots.optionsSlot[0].children[0].text).toMatch(
       "Hello World"
     );
   });
