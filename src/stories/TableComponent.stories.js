6751f77ca8c9b697b67b6710981abe561fc16ffd
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-08-28 11:43:49 +0530

    refactor: updated table header props in storybook

--- src/stories/TableComponent.stories.js
+++ src/stories/TableComponent.stories.js
@@ -9,12 +9,54 @@ export default {
   render: (args, { argTypes }) => ({
     components: { TableComponent },
     props: Object.keys(argTypes),
-    template: "<table-component :userData='userData'/>",
+    template: "<table-component v-bind='$props'/>",
   }),
 };
 
 export const Table = {
   args: {
     userData: UserDetails.userDetails.users,
+    tableHeader: [
+      {
+        text: "Name",
+        sortable: true,
+        value: "name",
+        align: "start",
+      },
+      {
+        text: "Company",
+        sortable: true,
+        value: "company",
+        align: "start max-[447px]:hidden",
+      },
+      {
+        text: "Status",
+        sortable: true,
+        value: "status",
+        align: "start max-[726px]:hidden",
+      },
+      {
+        text: "Assigned to",
+        sortable: true,
+        value: "assignedTo",
+        align: "start max-[616px]:hidden",
+      },
+      {
+        text: "Phone",
+        sortable: true,
+        value: "phone",
+        align: "start max-[854px]:hidden",
+      },
+      {
+        text: "Email",
+        sortable: true,
+        value: "email",
+        align: "start max-[1372px]:hidden",
+      },
+      {
+        text: "",
+        value: "data-table-expand",
+      },
+    ],
   },
 };
