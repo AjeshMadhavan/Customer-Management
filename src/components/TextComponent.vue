b86dafbe5bb435ad121245b5a2007f927a6c6523
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-09-04 18:01:46 +0530

    refactor: updated props and added props default values

--- src/components/TextComponent.vue
+++ src/components/TextComponent.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="flex items-center">
-    <v-icon v-if="props.prependIcon" class="!text-base/4 !text-slate-400 p-1.5">
+    <v-icon
+      v-if="props.prependIcon"
+      :class="['!text-base/4', '!text-slate-400', 'p-1.5', prependIconStyle]"
+    >
       {{ props.prependIcon }}
     </v-icon>
-    <div :class="['font-normal', 'list-text', props.textContainerStyle]">
-      <span v-if="props.text" class="text-2xs">
-        {{ props.text }}
+    <div
+      :class="[
+        'font-normal',
+        'text-container',
+        { 'flex-col !items-start': isDirectionColumn },
+      ]"
+    >
+      <span v-if="props.label" class="text-2xs pr-1">
+        {{ props.label }}
       </span>
-      <span v-if="props.linkText" :class="['text-xs', props.linkTextStyle]">
-        {{ props.linkText }}
+      <span v-if="props.text" :class="['text-xs', props.textStyle]">
+        {{ props.text }}
       </span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { defineProps, withDefaults } from "vue";
 
 interface Props {
+  isDirectionColumn?: boolean;
+  label?: string;
   prependIcon?: string;
-  text?: string;
-  linkText?: string;
-  linkTextStyle?: string;
-  textContainerStyle?: string;
+  prependIconStyle: string;
+  text: string;
+  textStyle?: string;
 }
 
-const props = defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), {
+  isDirectionColumn: false,
+  label: "",
+  prependIcon: "",
+  prependIconStyle: "",
+  textStyle: "",
+});
 
 //<text-component prependIcon="mdi-phone" textContainerStyle="px-3 py-2.5"  linkText="CTO" />
 //<text-component textContainerStyle="px-3 pb-1" text="Position" linkText="CTO" linkTextStyle="text-blue-600"/>
 </script>
 
 <style scoped lang="scss">
-.list-text {
+.text-container {
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
+  align-items: center;
   row-gap: 5px;
   font-weight: 400;
 }
