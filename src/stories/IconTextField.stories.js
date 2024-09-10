37d88881dbf6b4516a1f232659eb3dd101556615
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-09-10 11:57:57 +0530

    refactor: updated iconTextField component storybook

--- src/stories/IconTextField.stories.js
+++ src/stories/IconTextField.stories.js
@@ -4,47 +4,36 @@ export default {
   component: IconTextField,
   tags: ["autodocs"],
   argTypes: {
-    text: {
-      control: "text",
+    isVertically: {
+      control: "boolean",
     },
-    linkText: {
-      control: "text",
-    },
-    linkTextStyle: {
-      control: "text",
-    },
-    textContainerStyle: {
+    label: {
       control: "text",
     },
     prependIcon: {
+      control: "text",
+    },
+    prependIconStyle: {
+      control: "text",
+    },
+    text: {
+      control: "text",
+    },
+    textStyle: {
       control: "text",
     },
   },
   render: (args, { argTypes }) => ({
     components: { IconTextField },
     props: Object.keys(argTypes),
-    template: `<icon-text-field 
-                :text="text" 
-                :linkText="linkText" 
-                :linkTextStyle="linkTextStyle"
-                :textContainerStyle="textContainerStyle"
-                :prependIcon="prependIcon"        
-        />`,
+    template: `<icon-text-field v-bind="$props"/>`,
   }),
 };
 
-export const TextwithLink = {
+export const iconTextField = {
   args: {
-    text: "Position",
-    linkText: "CTO",
-    linkTextStyle: "text-cyan-400",
-  },
-};
-
-export const TextWithIcon = {
-  args: {
-    linkText: "+1(213)555-4276",
-    prependIcon: "mdi-phone",
-    textContainerStyle: "px-3 pb-1",
+    label: "Position",
+    text: "CTO",
+    textStyle: "text-cyan-400",
   },
 };
