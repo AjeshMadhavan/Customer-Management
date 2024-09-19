8183f22ceee6228c1b914d3dd559cb52d64e2015
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-09-19 12:13:26 +0530

    feat: added user details container

--- src/pages/ContactListPage.vue
+++ src/pages/ContactListPage.vue
@@ -1,25 +1,99 @@
 <template>
-  <div class="w-full">
-    <header-container />
-    <div class="flex">
+  <div class="w-full h-full">
+    <header-container class="z-10" />
+    <div class="flex relative">
       <div class="sidebar-container">
         <sidebar-container />
       </div>
-      <contact-list-container />
+      <contact-list-container
+        :table-data="UserDetailsData.userDetails.users"
+        :table-header="tableHeader"
+        @row-click="handleTableRowClick"
+      />
+      <div
+        :class="['user-details-container bg-white', { '!right-0': userData }]"
+      >
+        <user-details :user-data="userData" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import UserDetails from "@/Json/UserDetails.json";
+import { ref } from "vue";
 
-import HeaderContainer from "@/containers/HeaderSection.vue";
+import UserDetailsData from "@/Json/UserDetails.json";
+
+import { UserData } from "@/Type/Types";
+
+import HeaderContainer from "@/containers/HeaderContainer.vue";
 import SidebarContainer from "@/containers/SidebarContainer.vue";
 import ContactListContainer from "@/containers/ContactListContainer.vue";
+import UserDetails from "@/containers/UserDetails.vue";
+
+const userData = ref<UserData | undefined>(undefined);
+
+const tableHeader = [
+  {
+    text: "Name",
+    sortable: true,
+    value: "name",
+    align: "start",
+  },
+  {
+    text: "Company",
+    sortable: true,
+    value: "company",
+    align: "start max-[447px]:hidden",
+  },
+  {
+    text: "Status",
+    sortable: true,
+    value: "status",
+    align: "start max-[726px]:hidden",
+  },
+  {
+    text: "Assigned to",
+    sortable: true,
+    value: "assignedTo",
+    align: "start max-[616px]:hidden",
+  },
+  {
+    text: "Phone",
+    sortable: true,
+    value: "phone",
+    align: "start max-[854px]:hidden",
+  },
+  {
+    text: "Email",
+    sortable: true,
+    value: "email",
+    align: "start max-[1372px]:hidden",
+  },
+  {
+    text: "",
+    value: "data-table-expand",
+  },
+];
+
+const handleTableRowClick = (TableRowData: UserData) => {
+  userData.value = TableRowData;
+};
 </script>
 
 <style scoped lang="scss">
 .sidebar-container {
   width: 250px;
+  position: relative;
+  left: 0;
+}
+
+.user-details-container {
+  position: absolute;
+  right: -350px;
+  top: 0;
+  bottom: 0;
+  transition: right 0.4s ease;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.04), 0 4px 4px 0 rgba(0, 0, 0, 0.12);
 }
 </style>
