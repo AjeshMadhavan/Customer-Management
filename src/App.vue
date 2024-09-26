899878d8e8b0e36ecfbb054f8fa0a702fe78d2cf
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-09-26 19:45:20 +0530

    refactor: updated sidebar container styles

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import ContactList from '@/pages/ContactList.vue'
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
